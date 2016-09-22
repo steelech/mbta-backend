d1007c02e4cd81e94a76c31d6d28ccdf83b5d52f
Charlie Steele <steelech@umich.edu>
2016-09-22 19:18:16 -0400

    Test clock for Heroku

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,6 +5,7 @@ require './config/environment'
 module Clockwork
 	every(60.seconds, "get data") do 
 		puts "getting data"
+		puts "doing stuff"
 		DatabaseCleaner.delay.clean_with(:truncation, :only => ['routes', 'trips', 'stops'])
 		MbtaService.delay.get_schedule
 	end
